3cf23a58b66adfa13a2b3ff12cdb5038e981bb4c
Shreya Malviya <malviyashreya26@gmail.com>
2021-07-11 11:16:05 +0530

    [Shreya] Add: Refactor the Code to write a function to get work hours

--- EmpWagwProblem.js
+++ EmpWagwProblem.js
@@ -5,24 +5,32 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-let empHrs = 0;
-//Math.random() provides a random number from 0,1 ) 
+//let empHrs = 0;
+//Math.random() provides a random number from 0,1,2 ) 
 let empCheck = Math.floor(Math.random() * 3) ;
+//Functions are a great way to reuse code.
+// function for  getworkinghours( parameter)
+function GetWorkingHours(empCheck){
 switch (empCheck) {
-    case IS_ABSENT:
-        
-        empHrs = 0;
+    case IS_ABSENT:   //0    
+       return 0; // 0
         break;
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS; //const PART_TIME_HOURS = 4;
+    case IS_PART_TIME: //1
+        return PART_TIME_HOURS; //const PART_TIME_HOURS = 4;
         break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS; //const PART_TIME_HOURS = 8;
+    case IS_FULL_TIME: //2
+        return FULL_TIME_HOURS; //const PART_TIME_HOURS = 8;
         break;
     default:
-        break;
+        return  null; //null 'no value' or nonexistence of any value
 }
+}
+//0,1,2
+console.log(empCheck);
 
+let empHrs = GetWorkingHours(empCheck);
+// 0,4 , 8
+console.log(empHrs);
 let empWage = empHrs * WAGE_PER_HOUR; //const WAGE_PER_HOUR = 20;
 console.log("Emp Wage: " + empWage);
 
